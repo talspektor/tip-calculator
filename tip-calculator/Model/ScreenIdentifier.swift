f61dae4d610508534f7171e9e38c67abfdbce805
tspektor <tal_spektor@intuit.com>
2023-11-06 11:30:55 +0200

    UIText

---
+++ tip-calculator/Model/ScreenIdentifier.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenIdentifier.swift
+//  tip-calculator
+//
+//  Created by Tal Spektor on 06/11/2023.
+//
+
+import Foundation
---
+++ tip-calculatorUITests/CalculatorScreen.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorScreen.swift
+//  tip-calculatorUITests
+//
+//  Created by Tal Spektor on 06/11/2023.
+//
+
+import Foundation
